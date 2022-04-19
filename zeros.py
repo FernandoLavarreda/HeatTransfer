6203e1109ac6e9d83d0dc998c19f0261bb167a92
Fernando José Lavarreda Urizar <fernandolavarredau@gmail.com>
2022-04-19 09:19:15 -0600

    Documentation for zeros.py

--- zeros.py
+++ zeros.py
@@ -1,54 +1,92 @@
+"""
+Fernando Jose Lavarreda Urizar
+Funtions to find lambdas for Transient Heat Conduction
+Functions for spheres, cylinders and walls
+
+Example of call to the program:
+py 
+"""
 
 import sys
+from typing import List, Callable
 from functools import partial
 import matplotlib.pyplot as plt
 from math import sin, cos, tan, factorial, pi
 
-def pared(lamb, biot):
+
+def pared(lamb:float, biot:float)->float:
+    """Function that defines the relationship between the biot and lambda for a wall"""
     return lamb*tan(lamb)-biot
 
 
-def dpared(lamb):
+def dpared(lamb:float)->float:
+    """Derivative of pared function"""
     return tan(lamb)+lamb*1/(cos(lamb)**2)
 
 
-def cilindro(lamb, biot):
+def cilindro(lamb:float, biot:float)->float:
+    """Function that defines the relationship between biot and lamda for a cylinders"""
     return lamb*(bessel(lamb, 1)/bessel(lamb, 0))-biot
 
 
-def dcilindro(lamb):
+def dcilindro(lamb:float)->float:
+    """Derivative of cilindro function"""
     return (bessel(lamb, 1)/bessel(lamb, 0))+lamb*derivar(lamb, lambda x: (bessel(x, 1)/bessel(x, 0)))
 
 
-def esfera(lamb, biot):
+def esfera(lamb:float, biot:float)->float:
+    """Function that defines the relationship between biot anda lamda for a sphere"""
     return 1-lamb/tan(lamb)-biot
 
 
 def desfera(lamb):
+    """Derivative of esfera function"""
     return -1/tan(lamb)+lamb/(sin(lamb)**2)
 
 
-def newtons(fn, dx, xo, tolerancia=0.000001):
+def newtons(fn:Callable[[float], float], dx:Callable[[float], float], xo:float, tolerancia:float=0.000001, maxiter:int=10000)->List[float]:
+    """Definition of Newton's Method
+    fn: Function that requires that takes as an argument a float value to evaluate
+    dx: Function is the derivative from fn that requires that takes as an argument a float value to evaluate
+    xo: Value where to start evaluation to find a zero
+    tolerancia: acceptable error for the zero found, defined as distance of function(zero) to 0
+    maxiter: maximum value of iterations before exiting function if zero has not been found Default value set to 10000 iterations, raises StopIteration
+    returns a tuple with the zero and number of iterations
+    """
     i = 0
     while abs(fn(xo))>tolerancia:
         xo -= fn(xo)/dx(xo)
         i+=1
+        if i>=maxiter:
+            raise StopIteration("Maximum number of iterations reached and no zero was found")
     return xo, i
 
 
-def multiple_newtons(fn, dx, xo, number, step, tolerancia=0.000001):
+def multiple_newtons(fn:Callable[[float], float], dx:Callable[[float], float], xo:float, number:int, step:float, tolerancia:float=0.000001, maxiter:int=10000)->List[float]:
+    """Function to obtain multiple zeros from a function
+       for fn, dx, xo, tolerancia check newtons documentation
+       number: number of zeros to be found on the function
+       step: distance from last zero to check for the next zero
+       returns list with the zeros found on the function
+       raises StopIteration if max iteration for a zero was reached
+    """
     found = []
     for i in range(number):
         if found:
-            zero, iteration = newtons(fn, dx, found[-1]+step, tolerancia=tolerancia)
+            zero, iteration = newtons(fn, dx, found[-1]+step, tolerancia=tolerancia, maxiter=maxiter)
         else:
-            zero, iteration = newtons(fn, dx, xo, tolerancia=tolerancia)
+            zero, iteration = newtons(fn, dx, xo, tolerancia=tolerancia, maxiter=maxiter)
         found.append(zero)
     return found
 
 
 
-def bessel(x, degree, terms=120):
+def bessel(x:float, degree:int, terms=120)->float:
+    """Definition of Bessel function
+       x: point of evaluation for bessel function. For 120 terms 35 is max value to evaluate with semi accurate results
+       degree: degree of bessel function to evaluate. Value shouldn't be more than 10/11 for a regular computer
+       terms: number of terms to use in bessel function calculation. Big terms value may cause OverflowError
+    """
     if x>35:
         return sum([(x/2)**(i)*((-1)**i)/factorial(i)*(x/2)**(degree)/factorial(i+degree)*(x/2)**(i) for i in range(terms+48)])
     else:
@@ -56,7 +94,12 @@ def bessel(x, degree, terms=120):
 
 
 
-def derivar(x, func, diferencial=1e-8):
+def derivar(x:float, func:Callable[[float], float], diferencial:float=1e-8)->float:
+    """Derivation of a function be numeric methods
+       x: point to derivate the function
+       func: function to derivate, takes a float argument
+       diferencial: half the distance between points of evaluation to obtain derivative. This is the delta
+    """
     return (func(x+diferencial)-func(x-diferencial))/(2*diferencial)
 
 
